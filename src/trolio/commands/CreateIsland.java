99685026870f1a866b925d6afec815fb125ef3b2
trolio <42102227+trolio@users.noreply.github.com>
2019-12-01 00:45:31 -0500

    reset of CreateWorld Class.

--- src/trolio/commands/CreateIsland.java
+++ src/trolio/commands/CreateIsland.java
@@ -1,14 +1,8 @@
 package trolio.commands;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.UUID;
-
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import net.md_5.bungee.api.ChatColor;
 import trolio.interfaces.CommandInterface;
 
 public class CreateIsland implements CommandInterface
@@ -19,14 +13,7 @@ public class CreateIsland implements CommandInterface
 	@Override
 	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args)
 	{
-		Player player = (Player) sender;
 		
-		if (args.length > 1)
-		{
-			return false;
-		}
-		
-		player.sendMessage(ChatColor.GOLD + "Creating Island!");
 		return false;
 	}
 }
