5dbf484f2783a5d462989169b8d798d8114fdc77
trolio <42102227+trolio@users.noreply.github.com>
2019-12-01 02:07:54 -0500

    .

--- src/trolio/commands/CreateIsland.java
+++ src/trolio/commands/CreateIsland.java
@@ -1,16 +1,16 @@
 package trolio.commands;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.UUID;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import org.apache.commons.io.FileUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import trolio.interfaces.CommandInterface;
@@ -18,42 +18,53 @@ import trolio.interfaces.CommandInterface;
 public class CreateIsland extends JavaPlugin implements CommandInterface
 {
 	
-	World world;
+	private static void copyFileStructure(File source, File target)
+	{
+		try
+		{
+			ArrayList ignore = new ArrayList<>(Arrays.asList("uid.dat", "session.lock"));
+			
+			if (!ignore.contains(source.getName()))
+			{
+				if (source.isDirectory())
+				{
+					if (!target.exists())
+						if (!target.mkdirs())
+							throw new IOException("Could not create world directory");
+					
+					String files[] = source.list();
+					
+					for (String file : files)
+					{
+						File srcFile = new File (source, file);
+						File destFile = new File (target, file);
+						copyFileStructure(srcFile, destFile);
+					}
+				}
+				else
+				{
+					InputStream in = new FileInputStream(source);
+					OutputStream out = new FileOutputStream(target);
+					byte[] buffer = new byte[1024];
+					int length;
+					
+					while ((length = in.read(buffer)) > 0)
+						out.write(buffer, 0, length);
+					in.close();
+					out.close();
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	
 	
 	@Override
 	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args)
 	{
-		
-		Player player = (Player) sender;
-		
-		UUID uuid = player.getUniqueId();
-		String playerUUID = uuid.toString();
-		
-		File dataFolder = new File(this.getDataFolder().getPath());
-		String strData = dataFolder.toString();
-		String[] split = strData.toString().split(File.pathSeparator);
-		String rootFolder = split[split.length - 3];
-		File root = new File(rootFolder);
-		
-		File srcDir = new File (root+File.pathSeparator+"backup");
-		
-		if (!srcDir.exists())
-		{
-			Bukkit.getLogger().warning("Backup does not exist!");
-		}
-		
-		File destDir = new File (root+File.pathSeparator+"skyworld");
-		
-		try {
-			FileUtils.copyDirectory(srcDir, destDir);
-		}
-		catch (IOException ex)
-		{
-			ex.printStackTrace();
-		}
-		
-		Bukkit.getServer().createWorld(new WorldCreator(playerUUID));
-		
 		return false;
 	}
 }
