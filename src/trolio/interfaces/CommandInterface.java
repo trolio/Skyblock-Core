463f8a4510251631e1ea2a85bf50e72031f3ecee
trolio <42102227+trolio@users.noreply.github.com>
2019-12-01 04:53:29 -0500

    removed for complete redesign.

--- src/trolio/interfaces/CommandInterface.java
+++
@@ -1,9 +0,0 @@
-package trolio.interfaces;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface CommandInterface 
-{
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args);
-}
