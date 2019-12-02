de02e4569d0efcbba2b7b9d5cefaaed7e5e71f72
trolio <42102227+trolio@users.noreply.github.com>
2019-12-02 01:45:14 -0500

    added ability to visit other players island but not visit your own.

--- src/trolio/commands/IslandVisit.java
+++ src/trolio/commands/IslandVisit.java
@@ -1,48 +1,79 @@
 package trolio.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import net.md_5.bungee.api.ChatColor;
 import trolio.Main;
 
 public class IslandVisit implements SkyCommand
 {
 	
-	private Player target;
-	private Player owner;
-
+	public Player target;
+	public Player targetOnline;
+	public OfflinePlayer targetOffline;
+	
+	@SuppressWarnings("deprecation")
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+		
+		Player player = (Player) sender;
+		
 		if (sender instanceof Player)
 		{	
-			String targetPlayer = args[0];
+			String targetPlayer = args[1];
+			String cmdSender = sender.getName();
 			
-			target = Bukkit.getServer().getPlayer(targetPlayer);
+			targetOnline = Bukkit.getServer().getPlayer(args[1]);
+			targetOffline = Bukkit.getOfflinePlayer(targetPlayer);
+			
+			if (targetPlayer == cmdSender)
+			{
+				player.sendMessage(ChatColor.RED + "You can not visit your own island!");
+			}
+			else
+			{
+				if (!(targetOnline == null))
+				{
+					player.teleport(getPlayerIslandOnline());
+				}
+				else if (targetOnline == null)
+				{
+					player.teleport(getPlayerIslandOffline());
+				}
+			}
 		}
 		return false;
 	}
 	
-	private Location getIslandLocation()
+	private Location getIslandLocationOnline()
 	{
 		return new Location (Main.skyWorld,
-				Main.config.getDouble("Islands." + this.target.getUniqueId() + ".x"),
-				Main.config.getDouble("Islands." + this.target.getUniqueId() + ".y"),
-				Main.config.getDouble("Islands." + this.target.getUniqueId() + ".z"));
+				Main.config.getDouble("Islands." + this.targetOnline.getUniqueId() + ".x"),
+				Main.config.getDouble("Islands." + this.targetOnline.getUniqueId() + ".y"),
+				Main.config.getDouble("Islands." + this.targetOnline.getUniqueId() + ".z"));
 	}
 	
-	public Location getPlayerIsland()
+	private Location getIslandLocationOffline()
 	{
-		return getIslandLocation().add(2, 4, 2);
+		return new Location (Main.skyWorld,
+				Main.config.getDouble("Islands." + this.targetOffline.getUniqueId() + ".x"),
+				Main.config.getDouble("Islands." + this.targetOffline.getUniqueId() + ".y"),
+				Main.config.getDouble("Islands." + this.targetOffline.getUniqueId() + ".z"));
 	}
 	
-	public void tpIsland()
+	public Location getPlayerIslandOnline()
 	{
-		this.owner.sendMessage(ChatColor.GOLD + "Sending you to island!");
-		this.owner.teleport(getPlayerIsland());
+		return getIslandLocationOnline().add(2, 4, 2);
 	}
 	
+	public Location getPlayerIslandOffline()
+	{
+		return getIslandLocationOffline().add(2, 4, 2);
+	}
 }
